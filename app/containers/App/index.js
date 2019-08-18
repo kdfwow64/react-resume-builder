f9b5733719fd56dd22cdd13ab7d3d48c82cb2921
Ghazanfar <ghazanfar89@gmail.com>
2019-08-18 22:10:55 +0500

    Adding feature

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,50 +1,22 @@
-/**
- *
- * App
- *
- * This component is the skeleton around the actual pages, and should only
- * contain code that should be seen on all pages. (e.g. navigation bar)
- */
-
 import React from 'react';
-import { Helmet } from 'react-helmet';
-import styled from 'styled-components';
-import { Switch, Route } from 'react-router-dom';
+import { Grid, makeStyles } from '@material-ui/core';
+import MenuAppBar from 'components/Appbar';
+import Tabs from 'components/Tabs';
 
-import HomePage from 'containers/HomePage/Loadable';
-import FeaturePage from 'containers/FeaturePage/Loadable';
-import NotFoundPage from 'containers/NotFoundPage/Loadable';
-import Header from 'components/Header';
-import Footer from 'components/Footer';
-
-import GlobalStyle from '../../global-styles';
-
-const AppWrapper = styled.div`
-  max-width: calc(768px + 16px * 2);
-  margin: 0 auto;
-  display: flex;
-  min-height: 100%;
-  padding: 0 16px;
-  flex-direction: column;
-`;
-
+const useStyles = makeStyles(theme => ({
+  main: {
+    backgroundColor: '#f7f7f7' || theme.palette.grey,
+    minHeight: '100vh',
+  },
+}));
 export default function App() {
+  const classes = useStyles();
   return (
-    <AppWrapper>
-      <Helmet
-        titleTemplate="%s - React.js Boilerplate"
-        defaultTitle="Resume Builder"
-      >
-        <meta name="description" content="A React.js Boilerplate application" />
-      </Helmet>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/features" component={FeaturePage} />
-        <Route path="" component={NotFoundPage} />
-      </Switch>
-      <Footer />
-      <GlobalStyle />
-    </AppWrapper>
+    <Grid container justify="center" className={classes.main}>
+      <Grid item xs={10} md={11}>
+        <MenuAppBar />
+        <Tabs />
+      </Grid>
+    </Grid>
   );
 }
