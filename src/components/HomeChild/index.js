c794c4a90d9e836ed35f79139398599301b5902e
Ghazanfar <ghazanfar89@gmail.com>
2019-09-04 17:23:48 +0500

    Added main page

--- src/components/HomeChild/index.js
+++ src/components/HomeChild/index.js
@@ -4,26 +4,116 @@
  *
  */
 
-import React, { memo, useState, useEffect } from "react";
+import React, { memo } from "react";
 import { Grid, Box, Typography, Paper } from "@material-ui/core";
-import { headingChildStyles, homeChildStyles } from "./style";
+import { homeChildStyles } from "./style";
 import CustomButton from "../Button";
 import { Link } from "react-router-dom";
-
+import CV from "../../assets/cv.png";
 import { useSelector, useDispatch } from "react-redux";
 import { AttachFile, FontDownload } from "@material-ui/icons";
+import MenuAppBar from "../Appbar/Appbar";
 
 function HomeChild() {
   const classes = homeChildStyles();
   return (
-    <Grid container className={classes.container} spacing={6} justify="center">
+    <Grid container spacing={6} justify="center">
+      <Grid item xs={12}>
+        <MenuAppBar variant="home" />
+      </Grid>
+      <Grid item md={11}>
+        <Grid container justify="space-between" alignItems="center">
+          <Grid item md={6}>
+            <Typography
+              className={[classes.title, classes.letterSpacing3]}
+              variant="h4"
+            >
+              Crowdsource your resume building
+            </Typography>
+            <Typography className={[classes.container]}>
+              Everyone using this site draws on a global database of current,
+              crowdsourced resume data (not the actual resume) to create a
+              personalized, easily editable resume. In turn, you are sharing
+              your resume content (minus your contacts/personal info) to further
+              update and improve the database.
+            </Typography>
+            <Box ml={7}>
+              <CustomButton
+                style={{ width: 250 }}
+                className={classes.container}
+              >
+                build your resume
+              </CustomButton>
+              <Typography className={classes.mt3}>
+                Sign-up FREE. No Credit Card Required.
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid item md={5}>
+            <img src={CV} style={{ height: 740, width: "100%" }} />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item xs={10} className={classes.container}>
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+          <Typography
+            className={[classes.title, classes.w60, classes.letterSpacing3]}
+            variant="h4"
+          >
+            BUILD YOUR RESUME IN MINUTES, EDIT IN SECONDS, APPLY ASAP
+          </Typography>
+          <Typography variant="caption">
+            Community driven resume builder{" "}
+          </Typography>
+        </Box>
+      </Grid>
       <Grid item xs={12} md={5}>
         <Box>
           <Box display="flex" alignItems="center">
             <AttachFile className={classes.icon} />
             <Typography
               variant="h5"
-              className={[classes.title, , classes.letterSpacing3]}
+              className={[classes.title, classes.letterSpacing3]}
+            >
+              Free{" "}
+            </Typography>
+          </Box>
+          <Box>
+            <Typography>
+              There is no hidden charges or "Premium" accounts. It is absolutely
+              free, no Credit Card required.
+            </Typography>
+          </Box>
+        </Box>
+      </Grid>
+      <Grid item xs={12} md={5}>
+        <Box>
+          <Box display="flex" alignItems="center">
+            <AttachFile className={classes.icon} />
+            <Typography
+              variant="h5"
+              className={[classes.title, classes.letterSpacing3]}
+            >
+              CROWDSOURCED CONTENT{" "}
+            </Typography>
+          </Box>
+          <Box>
+            <Typography>
+              Everyone who uses the site shares their resume content to help
+              themselves and others to build the resumes that can't be ignored
+              by recruiters.
+            </Typography>
+          </Box>
+        </Box>
+      </Grid>
+
+      <Grid item xs={12} md={5}>
+        <Box>
+          <Box display="flex" alignItems="center">
+            <AttachFile className={classes.icon} />
+            <Typography
+              variant="h5"
+              className={[classes.title, classes.letterSpacing3]}
             >
               No Writer's block
             </Typography>
--- src/containers/HomePage/styles.js
+++ src/containers/HomePage/styles.js
@@ -1,8 +1,7 @@
-import { makeStyles } from '@material-ui/core';
+import { makeStyles } from "@material-ui/core";
 
 export const headingStyles = makeStyles(theme => ({
-	main: {
-		backgroundColor: '#f7f7f7' || theme.palette.grey,
-		minHeight: '100vh'
-	}
+  main: {
+    minHeight: "100vh"
+  }
 }));
