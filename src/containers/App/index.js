bd5b87b4c7679dd5fee1e8eb0fbe3da791560dc6
Aki Sharipov <aki@multiaki.com>
2019-09-07 11:32:39 -0400

    refactor

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,24 +1,46 @@
 import React from "react";
-import { Switch, Route, Router } from "react-router-dom";
+import {Switch, Route, Router, withRouter} from "react-router-dom";
 import Builder from "../Builder";
 import Heading from "../Heading";
-import { WorkHistory } from "../WorkHistory";
-import { Skills } from "../Skills";
-import { Summary } from "../Summary";
-import { Finalize } from "../Finalize";
-import { Edu } from "../Edu";
+import {WorkHistory} from "../WorkHistory";
+import {Skills} from "../Skills";
+import {Summary} from "../Summary";
+import {Finalize} from "../Finalize";
+import {Edu} from "../Edu";
 import HomePage from "../HomePage";
+import MenuAppBar from "../../components/Appbar/Appbar";
+import {Box, Grid, Paper} from "@material-ui/core";
+import {builderStyles} from "../Builder/styles";
 
-export default function App() {
+export default withRouter(function App(props) {
+  const classes = builderStyles();
+  const displayImage = props.location.pathname !== "/finalize";
+  console.log(displayImage)
   return (
     <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/heading" render={ (props) => <Builder {...props} displayImage={true} component={Heading} ></Builder> } />
-      <Route exact path="/work-history" render={ (props) => <Builder {...props} displayImage={true} component={WorkHistory} ></Builder> } />
-      <Route exact path="/education" render={ (props) => <Builder {...props} displayImage={true} component={Edu} ></Builder> } />
-      <Route exact path="/skills" render={ (props) => <Builder {...props} displayImage={true} component={Skills} ></Builder> } />
-      <Route exact path="/summary" render={ (props) => <Builder {...props} displayImage={true} component={Summary} ></Builder> } />
-      <Route exact path="/finalize" render={ (props) => <Builder {...props} displayImage={false} component={Finalize} ></Builder> } />
+      <Route exact path="/" component={HomePage}/>
+      <Grid item xs={10} md={11}>
+        <MenuAppBar/>
+        <Paper className={classes.paper} elevation={0}>
+          <Grid container spacing={3}>
+            <Grid item md={displayImage ? 8 : 12}>
+              <Switch>
+                <Route exact path="/heading" render={(props) => <Builder {...props} component={Heading}/>}/>
+                <Route exact path="/work-history" render={(props) => <Builder {...props} component={WorkHistory}/>}/>
+                <Route exact path="/education" render={(props) => <Builder {...props} component={Edu}/>}/>
+                <Route exact path="/skills" render={(props) => <Builder {...props} component={Skills}/>}/>
+                <Route exact path="/summary" render={(props) => <Builder {...props} component={Summary}/>}/>
+                <Route exact path="/finalize" render={(props) => <Builder {...props} component={Finalize}/>}/>
+              </Switch>
+            </Grid>
+            <Grid item md={4} className={displayImage ? classes.visible : classes.hidden}>
+              <Box boxShadow={2} borderRadius={4}>
+                <img src='https://via.placeholder.com/300x450' className={classes.img} alt='cv'/>
+              </Box>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Grid>
     </Switch>
   );
-}
+})
--- src/containers/Builder/index.js
+++ src/containers/Builder/index.js
@@ -1,43 +1,27 @@
 import React from "react";
-import { Switch, Route, Router } from "react-router-dom";
+import {Switch, Route, Router} from "react-router-dom";
 import Heading from "../Heading";
-import { WorkHistory } from "../WorkHistory";
-import { Skills } from "../Skills";
-import { Summary } from "../Summary";
-import { Finalize } from "../Finalize";
-import { Edu } from "../Edu";
+import {WorkHistory} from "../WorkHistory";
+import {Skills} from "../Skills";
+import {Summary} from "../Summary";
+import {Finalize} from "../Finalize";
+import {Edu} from "../Edu";
 import MenuAppBar from '../../components/Appbar';
-import { Grid, Paper, Box } from '@material-ui/core';
-import { builderStyles } from './styles';
+import {Grid, Paper, Box} from '@material-ui/core';
 import HomePage from "../HomePage";
 
 export default function Builder(props) {
-    const classes = builderStyles();
-    const {displayImage, component} = props;
+  const {displayImage} = props;
   return (
-    <Grid container justify='center' className={classes.main}>
-        <Grid item xs={10} md={11}>
-            <MenuAppBar />
-            <Paper className={classes.paper} elevation={0}>
-                <Grid container spacing={3}>
-                    <Grid item md={displayImage ? 8 : 12}>
-                        <Switch>
-                            <Route exact path="/heading" component={component} />
-                            <Route exact path="/work-history" component={WorkHistory} />
-                            <Route exact path="/education" component={Edu} />
-                            <Route exact path="/skills" component={Skills} />
-                            <Route exact path="/summary" component={Summary} />
-                            <Route exact path="/finalize" component={Finalize} />
-                        </Switch>
-                    </Grid>
-                    <Grid item md={4} className={displayImage ? classes.visible : classes.hidden}>
-                        <Box boxShadow={2} borderRadius={4}>
-                            <img src='https://via.placeholder.com/300x450' className={classes.img} alt='cv' />
-                        </Box>
-                    </Grid>
-                </Grid>
-            </Paper>
-        </Grid>
+    <Grid item md={displayImage ? 8 : 12}>
+      <Switch>
+        <Route exact path="/heading" component={Heading}/>
+        <Route exact path="/work-history" component={WorkHistory}/>
+        <Route exact path="/education" component={Edu}/>
+        <Route exact path="/skills" component={Skills}/>
+        <Route exact path="/summary" component={Summary}/>
+        <Route exact path="/finalize" component={Finalize}/>
+      </Switch>
     </Grid>
   );
 }
